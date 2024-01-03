9e2bf53a2716c0b3ac5f0d8fcafc6a1b42f84273
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-01-03 03:43:39 +0200

    1001

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+str1 = "object"
+str2 = "oriented"
+str3 = "programming"
+str4 = "with"
+str5 = "Python"
+print(str1 + "-" + str2[0:6] + " " + str3 + " " + str4 + " " + str5)
