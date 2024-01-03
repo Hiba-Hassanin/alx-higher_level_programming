189276ea74a063ad42806bc0415a6f1d6dc4634c
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-01-03 03:13:39 +0200

    13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -7,7 +7,6 @@
  * @number: number to be inserted
  * Return: address of the new node, or NULL if it failed
  */
-
 listint_t *insert_node(listint_t **head, int number)
 {
     listint_t *new_node, *current;
