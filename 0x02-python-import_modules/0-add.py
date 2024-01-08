f3c0c930614a58eb6e29067ea95e5df77f779a77
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-01-09 01:59:02 +0200

    The addition result.

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,8 +2,7 @@
 
 from add_0 import add
 
-a, b = 1, 2
-result = add(a, b)
-
-output = "{a:d} + {b:d} = {result:d}"
-print(output.format(a=a, b=b, result=result))
+if __name__ == '__main__':
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
