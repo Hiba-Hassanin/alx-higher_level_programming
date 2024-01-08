d756ef26b0fca0d9d1ef5958564c3ad2e17e643d
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-01-08 23:56:40 +0200

    max_integer

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return (None)
+
+    max_value = float('-inf')
+    for num in my_list:
+        if num > max_value:
+            max_value = num
+    return (max_value)
