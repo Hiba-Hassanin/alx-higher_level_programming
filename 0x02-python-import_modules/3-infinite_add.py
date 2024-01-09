820cadddcd23b4f0419f5b6c4e6dde9a3b4734ee
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-01-09 02:36:56 +0200

    Number of argument

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 import sys
-
-def add_arguments(arguments):
-    result = 0
-
-    for argument in arguments:
-        result += int(argument)
-
-    print(result)
-
-if __name__ == "__main__":
-    arguments = sys.argv[1:]
-    add_arguments(arguments)
+if __name__ == '__main__':
+    sys.argv.pop(0)
+    total = 0
+    for number in sys.argv:
+        total += int(number)
+    print("{}".format(total))
