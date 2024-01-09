5ea0d5742fb5734c819f4dbe63888d316335fca4
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-01-09 02:29:28 +0200

    Number of argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
-import sys
 
 if __name__ == '__main__':
-    args = sys.argv[1:]
-    num_arguments = len(args)
+    import sys
 
-    print("{} {}{}{}".format(num_arguments, "argument" if num_arguments == 1 else "arguments", ":" if num_arguments > 0 else ".", "\n" if num_arguments > 0 else ""))
+    sys.argv.pop(0)
 
-    for i, argument in enumerate(args, start=1):
-        print("{}: {}".format(i, argument))
+    argvlength = len(sys.argv)
+    # The length of sys.argv gives the number of command-line arguments.
+
+    if (argvlength == 0):
+        # If there are no arguments, print "0 arguments.".
+        print("0 arguments.")
+    elif (argvlength == 1):
+        # If there is only one argument, print "1 argument:" and the argument itself.
+        print("1 argument:")
+        print("{:d}: {}".format(len(sys.argv), sys.argv[0]))
+    else:
+        # If there are multiple arguments, print "{:d} arguments:" and loop through the arguments.
+        print("{:d} arguments:".format(argvlength))
+        number = 1
+        for argument in sys.argv:
+            # Loop through the arguments and print each argument with its position.
+            print("{:d}: {}".format(number, argument))
+            number += 1
