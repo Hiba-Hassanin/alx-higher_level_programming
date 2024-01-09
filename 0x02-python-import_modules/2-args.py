60251ecb8ddf935024bb9c62336c1656287c6b32
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-01-09 02:25:11 +0200

    Number of argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     args = sys.argv[1:]
     num_arguments = len(args)
 
-    print("{} {}{}.".format(num_arguments, "argument" if num_arguments == 1 else "arguments", ':' if num_arguments > 0 else '.'))
+    print("{} {}{}{}".format(num_arguments, "argument" if num_arguments == 1 else "arguments", ":" if num_arguments > 0 else ".", "\n" if num_arguments > 0 else ""))
 
     for i, argument in enumerate(args, start=1):
         print("{}: {}".format(i, argument))
