0d4d6848b3a72dec6dd18567dea78b5da39dabfd
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-01-09 02:43:49 +0200

    Imports the variable a from the file

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-import importlib.util
+from main import a
 
-if __name__ == '__main__':
-    spec = importlib.util.spec_from_file_location('variable_load_5', './variable_load_5.py')
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-
-    print(module.a)
+if __name__ == "__main__":
+    print(a)
