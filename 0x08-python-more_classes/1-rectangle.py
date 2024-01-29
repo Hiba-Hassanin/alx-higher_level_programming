a3400ac7b6eb7c00a792e380014274095510edc9
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-01-30 00:04:58 +0200

    Class Rectangle.

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """
+Module 1-rectangle
 Defines a class Rectangle with private instance attributes width and height.
 """
 
 
 class Rectangle:
+    """
+    Defines a rectangle with private instance attributes width and height.
+    """
 
     def __init__(self, width=0, height=0):
-        """The Rectangle instance."""
+        """Initializes the Rectangle instance."""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """The width."""
+        """Retrieves the width."""
         return self.__width
 
     @width.setter
@@ -27,7 +31,7 @@ class Rectangle:
 
     @property
     def height(self):
-        """The height."""
+        """Retrieves the height."""
         return self.__height
 
     @height.setter
@@ -38,3 +42,12 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle(2, 4)
+    print(my_rectangle.__dict__)
+
+    my_rectangle.width = 10
+    my_rectangle.height = 3
+    print(my_rectangle.__dict__)
