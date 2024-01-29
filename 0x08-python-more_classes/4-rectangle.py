c9e83430d57c6553e6e8e26821976defd2e9ae74
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-01-30 00:14:39 +0200

    Class Rectangle.

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,1 +1,92 @@
 #!/usr/bin/python3
+"""
+Module 4-rectangle
+Defines a class Rectangle with private instance attributes width and height,
+and public instance methods for area, perimeter, and string representation.
+"""
+
+
+class Rectangle:
+    """
+    Defines a rectangle with private instance attributes width and height,
+    and public instance methods for area, perimeter, and string representation.
+    """
+
+    def __init__(self, width=0, height=0):
+        """Initializes the Rectangle instance."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieves the width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the width with error checking."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieves the height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the height with error checking."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculates the area of the rectangle."""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Calculates the perimeter of the rectangle."""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        """Returns a string representation of the rectangle."""
+        if self.width == 0 or self.height == 0:
+            return ""
+        return "\n".join(["#" * self.width] * self.height)
+
+    def __repr__(self):
+        """Returns a string representation of the rectangle for recreation."""
+        return "Rectangle({}, {})".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle(2, 4)
+    print(str(my_rectangle))
+    print("--")
+    print(my_rectangle)
+    print("--")
+    print(repr(my_rectangle))
+    print("--")
+    print(hex(id(my_rectangle)))
+    print("--")
+
+    # create new instance based on representation
+    new_rectangle = eval(repr(my_rectangle))
+    print(str(new_rectangle))
+    print("--")
+    print(new_rectangle)
+    print("--")
+    print(repr(new_rectangle))
+    print("--")
+    print(hex(id(new_rectangle)))
+    print("--")
+
+    print(new_rectangle is my_rectangle)
+    print(type(new_rectangle) is type(my_rectangle))
