ab95d9e3c263ea75e6844a7869915ecce5030d18
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-01-30 00:18:55 +0200

    Class Rectangle.

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -12,13 +12,13 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
-        """The Rectangle instance."""
+        """Initializes the Rectangle instance."""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """The width."""
+        """Retrieves the width."""
         return self.__width
 
     @width.setter
@@ -32,7 +32,7 @@ class Rectangle:
 
     @property
     def height(self):
-        """The height."""
+        """Retrieves the height."""
         return self.__height
 
     @height.setter
@@ -55,12 +55,37 @@ class Rectangle:
         return 2 * (self.width + self.height)
 
     def __str__(self):
-        """Returns a string representation."""
+        """Returns a string representation of the rectangle."""
         if self.width == 0 or self.height == 0:
             return ""
         return "\n".join(["#" * self.width] * self.height)
 
     def __repr__(self):
-        """Returns a string representation for recreation."""
+        """Returns a string representation of the rectangle for recreation."""
         return "Rectangle({}, {})".format(self.width, self.height)
 
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle(2, 4)
+    print(str(my_rectangle))
+    print("--")
+    print(my_rectangle)
+    print("--")
+    print(repr(my_rectangle))
+    print("--")
+    print(hex(id(my_rectangle)))
+    print("--")
+
+    # create new instance based on representation
+    new_rectangle = eval(repr(my_rectangle))
+    print(str(new_rectangle))
+    print("--")
+    print(new_rectangle)
+    print("--")
+    print(repr(new_rectangle))
+    print("--")
+    print(hex(id(new_rectangle)))
+    print("--")
+
+    print(new_rectangle is my_rectangle)
+    print(type(new_rectangle) is type(my_rectangle))
