f8cb1aaf9e3ccbba33ac70b5acbb8dadf6e94bff
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-01-30 22:09:02 +0200

    Python objects.

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    return ", ".join(["BestSchool"] * magic_string.call_count)
-
-magic_string.call_count = 0
+def magic_string(string=[]):
+    string += ["BestSchool"]
+    return ", ".join(string)
