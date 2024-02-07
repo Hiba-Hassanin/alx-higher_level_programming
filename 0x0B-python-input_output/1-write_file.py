87f8f35a4989829d90062ae49749da350eeda506
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-02-07 02:51:10 +0200

    Input/Output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
-def read_file(filename=""):
-    """ The function reads a text file and then prints it to stdout """
-    with open(filename, encoding='utf-8') as f:
-        print(f.read(), end="")
+import sys
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF8) and returns the number of characters written."""
+    with open(filename, mode='w', encoding='utf-8') as file:
+        file.write(text)
+        return len(text)
+
+if len(sys.argv) != 3:
+    print("Usage: ./1-write_file.py filename text", file=sys.stderr)
+    sys.exit(1)
+
+print(write_file(sys.argv[1], sys.argv[2]))
