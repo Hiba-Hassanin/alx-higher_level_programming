e9cc56d3f22485d400c993d49287f05600b46f0d
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-02-07 03:41:58 +0200

    Inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Class MyList inherits from list."""
+
+class MyList(list):
+    """A class that inherits from list and provides additional functionality."""
+    def print_sorted(self):
+        """Print the list sorted in ascending order."""
+        print(sorted(self))
