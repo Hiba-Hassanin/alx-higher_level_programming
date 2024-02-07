16608a660ed76a44e10ee7eca1ce1768e5a7b413
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-02-06 18:13:16 -0800

    6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Class BaseGeometry with public instance method area()."""
+
+
+class BaseGeometry:
+    """Empty class representing base geometry."""
+
+    def area(self):
+        """Calculate the area."""
+
+        raise Exception("area() is not implemented")
