3879670a72a7546f8e874bee48b81788c810fda7
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-02-07 02:47:25 +0200

    Input/Output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
 
-def write_file(filename="", text=""):
-    """
-    Write a string to a text file and return the number of characters written.
-
-    Args:
-        filename (str): The name of the text file to write to.
-        text (str): The string to write to the file.
-
-    Returns:
-        int: The number of characters written to the file.
-    """
-    with open(filename, mode='w', encoding='utf-8') as file:
-        return file.write(text)
+def read_file(filename=""):
+    """ The function reads a text file and then prints it to stdout """
+    with open(filename, encoding='utf-8') as f:
+        print(f.read(), end="")
