ec8fb979f1b8fa4294ad01265ae8e26791410d36
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-02-07 03:35:25 +0200

    Inheritance

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,8 +2,5 @@
 
 def read_file(filename=""):
     """ The function reads a text file and then prints it to stdout """
-    with open(filename, encoding='utf-8') as f:
-        print(f.read(), end="")
-
-if __name__ == "__main__":
-    read_file()
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read(), end='')
