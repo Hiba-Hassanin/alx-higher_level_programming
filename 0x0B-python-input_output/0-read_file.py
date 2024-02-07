1ff2a49f2b3431ca33c916bf23663e41a00f1184
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-02-07 02:36:48 +0200

    Input/Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -13,5 +13,3 @@ def read_file(filename=""):
     with open(filename, encoding='utf-8') as file:
         print(file.read(), end='')
 
-if __name__ == "__main__":
-    read_file()
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -13,6 +13,3 @@ def write_file(filename="", text=""):
     """
     with open(filename, mode='w', encoding='utf-8') as file:
         return file.write(text)
-
-if __name__ == "__main__":
-    write_file()
