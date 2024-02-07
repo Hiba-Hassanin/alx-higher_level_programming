c3c95ded07f2d1a42bb732b79d1d37e068e909be
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-02-07 02:38:37 +0200

    Input/Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,3 @@ def read_file(filename=""):
     """ function that reads a text file (UTF8) and prints it to stdout """
     with open(filename, encoding='utf-8') as f:
         print(f.read(), end="")
-
-if __name__ == "__main__":
-    read_file()
