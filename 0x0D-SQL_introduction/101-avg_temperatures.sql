e5f8b4f45bf013f4597f157630c71638897c1abb
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-03-15 00:50:52 +0200

    SQL

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,6 @@
+-- Display average temperature (Fahrenheit) by city ordered by temperature (descending)
+
+SELECT city, AVG(temperature) AS average_temperature_fahrenheit
+FROM table_name
+GROUP BY city
+ORDER BY average_temperature_fahrenheit DESC;
