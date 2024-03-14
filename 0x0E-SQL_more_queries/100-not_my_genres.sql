f57f08f5e7cc7bba33fed5b77f70a51fded4bb8a
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-03-15 01:45:20 +0200

    SQL

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,5 +1,4 @@
 -- List all genres not linked to the show Dexter from the hbtn_0d_tvshows database
-
 SELECT name
 FROM hbtn_0d_tvshows.tv_genres
 WHERE id NOT IN (
@@ -10,5 +9,5 @@ WHERE id NOT IN (
         FROM hbtn_0d_tvshows.tv_shows
         WHERE title = 'Dexter'
     )
-)
+) OR id IS NULL
 ORDER BY name ASC;
