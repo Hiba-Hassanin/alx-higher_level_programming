25fdb43241acd004616c618022838cbcc6c9de45
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-03-15 01:31:50 +0200

    SQL

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,6 @@
 -- List all shows with their corresponding genre IDs from the hbtn_0d_tvshows database
-SELECT a.title, b.genre_id
-FROM hbtn_0d_tvshows.tv_shows AS a
-LEFT JOIN hbtn_0d_tvshows.tv_show_genres AS b ON a.id = b.tv_show_id
-ORDER BY a.title ASC, b.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+       FROM tv_shows
+       LEFT OUTER JOIN tv_show_genres
+       ON tv_shows.id = tv_show_genres.show_id
+       ORDER BY tv_shows.title, tv_show_genres.genre_id;
