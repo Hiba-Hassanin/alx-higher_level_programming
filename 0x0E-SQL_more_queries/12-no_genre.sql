1a78fddc40d3ff6d5f815c3e40076c1a4b3bb032
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-03-15 01:32:46 +0200

    SQL

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,7 @@
 -- List all shows without a linked genre from the hbtn_0d_tvshows database
-SELECT a.title, b.genre_id
-FROM hbtn_0d_tvshows.tv_shows AS a
-LEFT JOIN hbtn_0d_tvshows.tv_show_genres AS b ON a.id = b.tv_show_id
-WHERE b.genre_id IS NULL
-ORDER BY a.title ASC, b.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+       FROM tv_shows
+       LEFT OUTER JOIN tv_show_genres
+       ON tv_shows.id = tv_show_genres.show_id
+       WHERE tv_show_genres.show_id IS NULL
+       ORDER BY tv_shows.title, tv_show_genres.genre_id;
