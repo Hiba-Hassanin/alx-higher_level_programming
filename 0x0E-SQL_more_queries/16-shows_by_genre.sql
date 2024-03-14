76c9e04d6423a3ffe7940cfb1729706f2a14a946
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-03-15 01:38:43 +0200

    SQL

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,8 @@
 -- List all shows and their linked genres from the hbtn_0d_tvshows database
 SELECT tv_shows.title, tv_genres.name
-FROM hbtn_0d_tvshows.tv_shows
-LEFT JOIN hbtn_0d_tvshows.tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-LEFT JOIN hbtn_0d_tvshows.tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, tv_genres.name ASC;
+       FROM tv_shows
+       LEFT OUTER JOIN tv_show_genres
+       ON tv_shows.id = tv_show_genres.show_id
+       	  LEFT OUTER JOIN tv_genres
+	  ON tv_show_genres.genre_id = tv_genres.id
+       ORDER BY tv_shows.title, tv_genres.name;
