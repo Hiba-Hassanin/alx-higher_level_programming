b643615523366475ad32ab5689c2668dada8a750
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-03-15 01:35:12 +0200

    SQL

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,7 @@
 -- List all genres and the number of shows linked to each from the hbtn_0d_tvshows database
-SELECT a.name AS genre, COUNT(b.tv_show_id) AS number_of_shows
-FROM hbtn_0d_tvshows.tv_genres AS a
-INNER JOIN hbtn_0d_tvshows.tv_show_genres AS b ON a.id = b.genre_id
-GROUP BY a.name
-ORDER BY number_of_shows DESC;
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_shows
+       FROM tv_show_genres
+       INNER JOIN tv_genres
+       ON tv_genres.id = tv_show_genres.genre_id
+       GROUP BY tv_genres.name
+       ORDER BY number_shows DESC;
--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,9 @@
 -- List all genres of the show Dexter from the hbtn_0d_tvshows database
-SELECT b.name
-FROM hbtn_0d_tvshows.tv_shows AS a
-JOIN hbtn_0d_tvshows.tv_show_genres AS c ON a.id = c.tv_show_id
-JOIN hbtn_0d_tvshows.tv_genres AS b ON c.genre_id = b.id
-WHERE a.title = 'Dexter'
-ORDER BY b.name ASC;
+SELECT tv_genres.name
+       FROM tv_genres
+       INNER JOIN tv_show_genres
+       ON tv_genres.id = tv_show_genres.genre_id
+       	  INNER JOIN tv_shows
+	  ON tv_shows.id = tv_show_genres.show_id
+	  WHERE tv_shows.title = 'Dexter'
+       ORDER BY tv_genres.name;
