f63c24bbfe60041e48b7e85c39469f29fbe70c58
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-03-15 01:54:30 +0200

    SQL

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,6 @@
+-- List all shows from hbtn_0d_tvshows_rate by their rating sum
+SELECT a.title, SUM(b.rate) AS rating
+FROM tv_shows AS a
+INNER JOIN tv_show_ratings AS b ON a.id = b.show_id
+GROUP BY a.title
+ORDER BY rating DESC;
