55f386270c64fa380fbfef4ac70ad28e28054d70
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-03-15 00:26:54 +0200

    SQL

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,2 @@
-CREATE TABLE IF NOT EXISTS first_table (
-  id INT,
-  name VARCHAR(256)
-);
+-- Creates a table called first_table.
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
