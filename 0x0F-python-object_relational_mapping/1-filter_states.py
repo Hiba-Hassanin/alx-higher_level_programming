5b07b3e9179899335b333ef1df8612923f1623a3
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-04-20 04:29:36 +0200

    Python-1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python3
-"""script that lists all states with a name starting with N (upper N) from
-the database hbtn_0e_0_usa"""
+"""Script to list all states with a name starting with N
+from the database hbtn_0e_0_usa"""
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    rows = cur.fetchall()
+    # Connect to MySQL server
+    connection = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                                 passwd=argv[2], db=argv[3], charset="utf8")
+
+    # Create a cursor object to interact with the database
+    cursor = connection.cursor()
+
+    # Execute SQL query to select states starting with 'N'
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    # Fetch all the rows that match the query
+    rows = cursor.fetchall()
+
+    # Iterate through each row and print it if the state name starts with 'N'
     for row in rows:
         if row[1][0] == 'N':
             print(row)
-    cur.close()
-    db.close()
+
+    # Close the cursor and database connection
+    cursor.close()
+    connection.close()
