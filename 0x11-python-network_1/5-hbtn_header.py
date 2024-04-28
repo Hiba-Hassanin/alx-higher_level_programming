18e43f49e72d346cc9f5e667c11b763b956d192e
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-04-28 22:19:31 +0200

    Network.

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -12,4 +12,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     response = requests.get(url)
-    print(r.headers.get("X-Request-Id"))
+    x_request_id = response.headers.get('X-Request-Id')
+
+    print(x_request_id)
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """
-Script that takes in a URL and an email address,
-sends a POST request to the passed URL with the email as
-a parameter, and finally displays the body of the response.
+Script takes in a URL and an email address,
+sends a POST request to the passed URL
+with the email as a parameter, and finally
+displays the body of the response.
 """
 
 import requests
@@ -10,7 +11,7 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = {'email': sys.argv[2]}
-    response = requests.post(url, data=email)
-    print("Your email is:", sys.argv[2])
+    val = {"email": sys.argv[2]}
+
+    response = requests.post(url, data=val)
     print(response.text)
