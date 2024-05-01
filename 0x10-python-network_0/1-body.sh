7aef19e72aedb4a7cfbf09f1417e39f2ec93d863
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-05-01 21:27:46 +0200

    Network.

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Description: Takes in a URL, sends a GET request to the URL, displays if status code is 200.
-curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q "200" && curl -s "$1"
+curl -sX GET $1 -L
