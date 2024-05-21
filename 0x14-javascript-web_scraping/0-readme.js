32ceb4d8c0d6bcd3d043c01cba2c8b43def4958f
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-05-22 01:44:24 +0200

    WS

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-// Import the 'fs' module to work with the file system
 const fs = require('fs');
 const file = process.argv[2];
 const content = process.argv[3];
 fs.writeFile(file, content, 'utf-8', function (err) {
   if (err) {
-  // If an error occurred while reading the file, log the error message
     console.log(err);
   }
 });
