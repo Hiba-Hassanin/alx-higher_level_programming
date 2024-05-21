bdfb78075c54cbebd821112bb2abf11f5e28a67e
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-05-22 01:54:09 +0200

    WS

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,49 +2,19 @@
 
 const request = require('request');
 
-const apiUrl = process.argv[2];
+const url = process.argv[2];
 const characterId = 18;
 
-function getFilmsCount(url) {
-  return new Promise((resolve, reject) => {
-    request.get(url, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else {
-        const filmsData = JSON.parse(body);
-        const moviesWithWedgeAntilles = filmsData.results.filter(film =>
-          film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-        );
-        resolve(moviesWithWedgeAntilles.length);
-      }
-    });
-  });
-}
+request(url, function (err, response, body) {
+  if (!err) {
+    const { results } = JSON.parse(body);
 
-function getAllFilmsCount(apiUrl) {
-  return new Promise((resolve, reject) => {
-    let count = 0;
+    // Using reduce to count movies with characterId 18
+    const count = results.reduce((count, film) => {
+      const hasCharacterWithId18 = film.characters.find((character) => character.endsWith(`/api/people/${characterId}/`));
+      return hasCharacterWithId18 ? count + 1 : count;
+    }, 0);
 
-    function getPageCount(url) {
-      request.get(url, (error, response, body) => {
-        if (error) {
-          reject(error);
-        } else {
-          const filmsData = JSON.parse(body);
-          count += filmsData.results.length;
-          if (filmsData.next) {
-            getPageCount(filmsData.next);
-          } else {
-            resolve(count);
-          }
-        }
-      });
-    }
-
-    getPageCount(apiUrl);
-  });
-}
-
-getAllFilmsCount(apiUrl)
-  .then(count => console.log(count))
-  .catch(error => console.error(error));
+    console.log(count);
+  }
+});
