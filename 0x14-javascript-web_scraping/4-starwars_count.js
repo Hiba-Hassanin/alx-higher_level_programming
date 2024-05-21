4f9b41980c0d5f9537767f2fe177ee2d546c4493
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-05-22 01:52:19 +0200

    WS

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,14 +5,46 @@ const request = require('request');
 const apiUrl = process.argv[2];
 const characterId = 18;
 
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const filmsData = JSON.parse(body);
-    const moviesWithWedgeAntilles = filmsData.results.filter(film =>
-      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-    );
-    console.log(moviesWithWedgeAntilles.length);
-  }
-});
+function getFilmsCount(url) {
+  return new Promise((resolve, reject) => {
+    request.get(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        const filmsData = JSON.parse(body);
+        const moviesWithWedgeAntilles = filmsData.results.filter(film =>
+          film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
+        );
+        resolve(moviesWithWedgeAntilles.length);
+      }
+    });
+  });
+}
+
+function getAllFilmsCount(apiUrl) {
+  return new Promise((resolve, reject) => {
+    let count = 0;
+
+    function getPageCount(url) {
+      request.get(url, (error, response, body) => {
+        if (error) {
+          reject(error);
+        } else {
+          const filmsData = JSON.parse(body);
+          count += filmsData.results.length;
+          if (filmsData.next) {
+            getPageCount(filmsData.next);
+          } else {
+            resolve(count);
+          }
+        }
+      });
+    }
+
+    getPageCount(apiUrl);
+  });
+}
+
+getAllFilmsCount(apiUrl)
+  .then(count => console.log(count))
+  .catch(error => console.error(error));
