04b19cb8fc1768e43f8129ae4dec587fe726c611
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-05-22 01:59:01 +0200

    WS

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const todos = JSON.parse(body);
+    const completedTasks = {};
+
+    todos.forEach(function (todo) {
+      if (todo.completed) {
+        if (completedTasks[todo.userId]) {
+          completedTasks[todo.userId]++;
+        } else {
+          completedTasks[todo.userId] = 1;
+        }
+      }
+    });
+
+    console.log(completedTasks);
+  } else {
+    console.error('Error requesting the API:', error);
+  }
+});
