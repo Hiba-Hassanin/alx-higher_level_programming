fe9796fab6b0cf0fca5d7cd52754dddeb7d128b1
Hiba-Hassanin <Hiba.Hassanin00@Gmail.com>
2024-05-22 02:06:34 +0200

    WS

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,33 +1,20 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-const movieId = process.argv[2];
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-request(apiUrl, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    const movie = JSON.parse(body);
-    const characterUrls = movie.characters;
-
-    let charactersProcessed = 0;
-    characterUrls.forEach(function (characterUrl, index) {
-      request(characterUrl, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-          const character = JSON.parse(body);
-          console.log(character.name);
-
-          charactersProcessed++;
-          if (charactersProcessed === characterUrls.length) {
-            // All characters have been processed
-            process.exit(0);
-          }
-        } else {
-          console.error('Error requesting character:', error);
-        }
-      });
-    });
-  } else {
-    console.error('Error requesting the Star Wars API:', error);
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
